1d040dee81cf5165ad8ea472c639c2bd94dd8e63
Brandon.Morford <brandon.morford@blackbaud.me>
2022-12-02 07:39:36 -0700

    Update to use explicit types

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Part 1
-var input = File.ReadLines("./input.txt");
+IEnumerable<string> input = File.ReadLines("./input.txt");
 
 int index = 0;
 List<int> sums = new();
 while (index < input.Count())
 {
-    var group = input.Skip(index).TakeWhile(x => x != string.Empty);
+    IEnumerable<string> group = input.Skip(index).TakeWhile(x => x != string.Empty);
     index += group.Count() + 1;
     sums.Add(group.Select(x => int.Parse(x)).Sum());
 }
