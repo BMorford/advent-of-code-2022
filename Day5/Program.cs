acf43a509cf8b8e036e92ae71b43cadb6e4d1b32
Brandon.Morford <brandon.morford@blackbaud.me>
2022-12-05 15:04:20 -0700

    Day 4 wip

---
+++ Day5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Part 1
+var input = File.ReadLines("./input.txt").Take(8).Select(x => x.Trim().Chunk(3));
+
+Queue<char>[] positions = ;
+foreach(var line in input)
+{
+    //positions.Add(new Queue<char> { })
+}
+
