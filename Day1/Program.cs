31cb72832d3272e29fa544b3b7a62f50bb8beffc
BMorford <22627049+BMorford@users.noreply.github.com>
2024-12-12 14:43:25 -0700

    Init

---
+++ Day1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Part 1
+var input = File.ReadLines("./input.txt");
+
+int index = 0;
+List<int> sums = new();
+while (index < input.Count())
+{
+    var group = input.Skip(index).TakeWhile(x => x != string.Empty);
+    index += group.Count() + 1;
+    sums.Add(group.Select(x => int.Parse(x)).Sum());
+}
+Console.WriteLine(sums.Max());
+
+// Part 2
+Console.WriteLine(sums.OrderByDescending(x => x).Take(3).Sum());
